7b5cac7a9491145e7144db83948ea7ce7b6f9f67
Philippe Chevreul <philippe.chevreul@paygreen.fr>
2022-06-07 11:29:54 +0200

    iso feature

--- src/lib/ListItem/style/constants.js
+++ src/lib/ListItem/style/constants.js
@@ -1,17 +1,15 @@
 import { math } from 'polished';
-
+// bulletFont Refactored in 16px base
 const bulletFont = {
-    xxs: '1rem',
-    xs: '1.1rem',
-    sm: '1.2rem',
-    md: '1.4rem',
-    lg: '1.6rem',
-    xl: '2rem'
+    xxs: '0.625rem',
+    xs: '0.6875rem',
+    sm: '0.75rem',
+    md: '0.875rem',
+    lg: '1rem',
+    xl: '1.25rem',
 };
 
-const marginBase = props => math(props.theme.icon.size[props.bulletSize] + '*0.75');
+const marginBase = props =>
+    math(props.theme.icon.size[props.bulletSize] + '*0.75');
 
-export {
-    bulletFont,
-    marginBase
-};
+export { bulletFont, marginBase };
